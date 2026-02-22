a00469eed70add40a06002f6527e2fd0ace035ad
Aseelmaradwi <kaseel134@gmail.com>
2026-02-22 23:48:43 +0200

    Add models

---
+++ job-backoffice/app/Models/Company.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Company extends Model
+{
+        use HasFactory, Notifiable,HasUuids,SoftDeletes;
+
+    protected $table = "companies";
+      protected $keyType="string";
+    public $incrementing = false;
+
+    protected $fillables = [
+        'name',
+        'address',
+        'industry',
+        'website',
+        'ownerId'
+    ];
+  
+      protected $dates = [
+        'deleted_at',
+    ];
+
+        protected function casts(): array
+    {
+        return [
+            'deleted_at'=> 'datetime',
+        ];
+    }
+
+public function owner(){
+    return $this->belongsTo(User::class,'ownerId','id');
+}
+
+
+
+}
---
+++ job-backoffice/app/Models/JobVacancy.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class JobVacancy extends Model
+{
+    use HasFactory, Notifiable,HasUuids,SoftDeletes;
+
+    protected $table = "job_vacancies";
+      protected $keyType="string";
+    public $incrementing = false;
+    
+
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'salary',
+        'salary',
+        'type',
+        'categoryId',
+        'companyId'
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+        protected function casts(): array
+    {
+        return [
+            'deleted_at'=> 'datetime',
+        ];
+    }
+
+    public function jobcategory()
+    {
+        return $this->belongsTo(JobCategory::class,'categoryId','id');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class,'companyId','id');
+    }
+
+    public function jobApplication(){
+        return $this->hasMany(JobApplication::class,'jobVacancyId','id');
+    }
+}
